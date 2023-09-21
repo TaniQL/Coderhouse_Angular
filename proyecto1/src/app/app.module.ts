0e7d5f3098eb22e475e4bba04276482210beb07b
TaniQL <taniquispelazo@gmail.com>
2023-09-21 03:06:19 -0500

    new components content, sidebar and topbar

---
+++ proyecto1/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TopbarComponent } from './topbar/topbar.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ContentComponent } from './content/content.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TopbarComponent,
+    SidebarComponent,
+    ContentComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgbModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
